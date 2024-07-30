482fa5b927294f5c38c55bf64be3ecf9aaaf10c4
Eleftherios Papadopoulos <lefteris.broker@gmail.com>
2024-07-30 16:02:56 +0200

    changes ok

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -4,7 +4,7 @@ domains=(kairos.gr www.kairos.gr)
 rsa_key_size=4096
 data_path="./certbot"
 email="lefteris.broker@gmail.com" # Change to your email
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
